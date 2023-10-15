9b525b866dbcd3a221d692f23c06c21211410c2f
Egor <142594624+ComRety@users.noreply.github.com>
2023-10-15 16:10:24 +0300

    вернул src

---
+++ src/js/read.js
@@ -0,0 +1,37 @@
+export function readAlls () {
+    let read = document.getElementById('inBlock');
+    read.style.height = 'auto';
+    readAll.style.display = 'none';
+    let readClose = document.getElementById('readClose');
+    readClose.style.display = 'inline';
+}
+
+let readAll = document.getElementById('readAll');
+readAll.addEventListener("click", readAlls);
+
+if (matchMedia) {
+    let screen = window.matchMedia("(min-width:768px)");
+    screen.addEventListener("change", closeText);
+    closeText(screen);
+}
+
+export function closeText(screen) {
+    if (screen.matches) {
+        let readClose = document.getElementById('readClose');
+        readClose.addEventListener("click", function() {
+        let read = document.getElementById('inBlock');
+        read.style.height = '160px';
+        readClose.style.display = 'none';
+        readAll = document.getElementById('readAll');
+        readAll.style.display = 'inline';
+    })} else {
+            let readClose = document.getElementById('readClose');
+            readClose.addEventListener("click", function() {
+            let read = document.getElementById('inBlock');
+            read.style.height = '100px';
+            readClose.style.display = 'none';
+            readAll = document.getElementById('readAll');
+            readAll.style.display = 'inline';
+})
+    }
+}
